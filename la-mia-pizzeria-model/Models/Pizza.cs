db39e9579051f0f62102388e1bb8897c32212406
adrianboh@gmail.com <adrianboh@gmail.com>
2023-04-07 11:09:55 +0200

    model

---
+++ la-mia-pizzeria-model/Models/Pizza.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public Pizza(string nome, string descrizione, string foto, double prezzo)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            Foto = foto;
+            Prezzo = prezzo;
+        }
+        [Key]
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Foto { get; set; }
+        public double Prezzo { get; set; }
+
+    }
+}
