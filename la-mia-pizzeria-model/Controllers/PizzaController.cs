7bfa824b4326411260b5fa866409cf8fb82d4318
adrianboh@gmail.com <adrianboh@gmail.com>
2023-04-07 11:07:08 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-model/Controllers/PizzaController.cs
@@ -0,0 +1,61 @@
+﻿using la_mia_pizzeria_model.Models;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet("Menu")]
+        public IActionResult Menu()
+        {
+            using var ctx = new Models.PizzeriaContext();
+            var pizze = ctx.Pizzas.ToArray();
+            return View(pizze);
+        }
+
+        [HttpGet("Contacts")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Details(long id)
+        {
+            using var ctx = new Models.PizzeriaContext();
+            var pizza = ctx.Pizzas.SingleOrDefault(p => p.Id == id);
+
+            if (pizza is null)
+            {
+                return NotFound($"Pizza with id {id} not found.");
+            }
+
+            return View(pizza);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ la-mia-pizzeria-model/Data/PizzeriaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using la_mia_pizzeria_static.Models;
+
+public class PizzeriaContext : DbContext
+{
+    public PizzeriaContext(DbContextOptions<PizzeriaContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<la_mia_pizzeria_static.Models.Pizza> Pizza { get; set; } = default!;
+}
